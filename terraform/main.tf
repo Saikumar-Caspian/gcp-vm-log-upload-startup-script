269281f6f9ba50d8f26aba38b172838ea5b0062b
Saikumar-Caspian <lukalapusaikumargov@gmail.com>
2026-01-17 14:50:41 +0000

    Phase T3: add storage bucket and service account with IAM

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+########################################
+# Cloud Storage Bucket for VM Logs
+########################################
+resource "google_storage_bucket" "vm_logs" {
+  name     = var.bucket_name
+  location = var.region
+
+  uniform_bucket_level_access = true
+}
+
+########################################
+# Service Account for VM Log Upload
+########################################
+resource "google_service_account" "vm_sa" {
+  account_id   = "tf-vm-log-uploader"
+  display_name = "Terraform VM Log Uploader Service Account"
+}
+
+########################################
+# IAM: Allow Service Account to Upload Logs
+########################################
+resource "google_storage_bucket_iam_member" "vm_sa_writer" {
+  bucket = google_storage_bucket.vm_logs.name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_storage_bucket_iam_member" "vm_sa_reader" {
+  bucket = google_storage_bucket.vm_logs.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.vm_sa.email}"
+}
